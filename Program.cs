ca2c71123b0d421656e21d1b86abc952e6eb8fd8
Kristin Caron <krissycaron@gmail.com>
2017-07-12 13:27:25 -0500

    first part of excersise complete

--- Program.cs
+++ Program.cs
@@ -8,12 +8,29 @@ namespace hashSet
         static void Main(string[] args)
         {
 
-           HashSet<string> showroom= new HashSet<string>();
-            showroom.Add("Audi");
-            showroom.Add("Kia");
-            showroom.Add("Cheverolet");
-            showroom.Add("Mazada");
+            HashSet<string> showroom = new HashSet<string>();
+                showroom.Add("Audi");
+                showroom.Add("Kia");
+                showroom.Add("Cheverolet");
+                showroom.Add("Mazada");
 
+                // Console.WriteLine(showroom.Count);
+
+                showroom.Add("Audi");
+                foreach (string carz in showroom){
+                    Console.WriteLine($"{carz}");
+                
+                }
+            
+            HashSet<string> usedCars = new HashSet<string>();
+                usedCars.Add("Ford");
+                usedCars.Add("Fiat");
+                
+
+            showroom.UnionWith(usedCars);
+                foreach (string carz in showroom){
+                    Console.WriteLine($"{carz}");
+                }
         }
     }
 }
